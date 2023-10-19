535256210e3af2419c9f1adcc533d5be4fb2cf85
D.OkenlaCOS101 <dvdokenla@gmail.com>
2023-10-19 19:36:34 +0100

    (Week 2 practice)

---
+++ week-2/practice_2.rs
@@ -0,0 +1,9 @@
+fn main() {
+	println!(); // prints nothing.
+
+	println!("hello "); // prints hello
+
+	println!("format {} arguments", "some"); // prints format some arguments
+
+	println!("My name is {}. I am the class rep of stream 1", "Dagogo Williams-Jumbo"); // prints My name is Dagogo Williams-Jumbo. I am the class rep of stream 1.
+}
